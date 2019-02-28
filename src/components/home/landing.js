515a2cdf78786a92b3f51c72f25e2e8af7ce9add
Edwin Agbenyega <eddy.devv@gmail.com>
2019-02-27 22:14:47 -0700

    more unnecessary experimentation with animations

--- src/components/home/landing.js
+++ src/components/home/landing.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react'
 import styled, { keyframes } from 'styled-components'
 
 const landingPage = {
-  "header" : {"id": 1, "type": "header", "content": "Welcome, We've Been Expecting You!"}
+  "header" : {"id": 1, "type": "header", "content": "Welcome to my page!"}
 }
 
 const moveHeader = keyframes`
-0% { color: red; }
-30% { color: white; }
-60% { color: black; }
-100% { color: purple; }
+0% { 
+  color: rgba(255,255,255,0.3); 
+  font-size: 2.3em;
+}
+25% { 
+  color: rgba(255,255,255,0.5); 
+}
+50% { 
+  color: rgba(255,255,255,0.7); 
+  font-size: 2.5em;
+}
+75% { 
+  color: rgba(255,255,255,0.5); 
+}
+100% { 
+  color: rgba(255,255,255,0.3); 
+  font-size: 2.3em;
+}
 `
 const LandingStyle = styled.div`
   font-size: 2.4em;
   color: white;
-  padding: 17%;
+  padding: 20%;
+  text-align: center;
   background-color: rgba(29,30,40,0.3);
   animation: ${moveHeader} linear 5s infinite;
   -webkit-animation: ${moveHeader} linear 5s infinite;
