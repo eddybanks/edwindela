fcd3eb5ad279379640b0b77aa9ec969311748a83
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-25 23:00:18 -0600

    Add entries to navigation list

--- src/components/layouts/navigation.js
+++ src/components/layouts/navigation.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+const navItems = [
+  { id: 0, title: 'Story', path: '/story' },
+  { id: 1, title: 'Music', path: '/music' },
+  { id: 2, title: 'Works', path: '/works' },
+  { id: 2, title: 'Art', path: '/art' },
+  { id: 2, title: 'Languages', path: '/languages' }
+]
+
 export default class Navigation extends Component {
   constructor(props) {
     super(props)
